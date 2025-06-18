edfba50e43b7b51af2c65914609730685b27643b
= <=>
2025-06-18 13:51:03 -0300

    fix: Correção no arquivo de resposta automática.

--- src/app/helpers/sendAutoReply.js
+++ src/app/helpers/sendAutoReply.js
@@ -29,10 +29,12 @@ module.exports = async function sendAutoReply(name, email, subject) {
             to: [email],
         },
         headers: {
-            "List-Unsubscribe": `<mailto:${emailSender}>`,
-            "Auto-Submitted": "auto-replied",
-            Precedence: "bulk",
+            "List-Unsubscribe": `<mailto:${emailSender}>`, // Permite ao cliente de e-mail exibir uma opção de "cancelar inscrição" (boas práticas de e-mail marketing/spam)
+            "Auto-Submitted": "auto-replied", // Informa que o e-mail foi gerado automaticamente (ajuda a evitar respostas automáticas)
+            Precedence: "bulk", // Indica que a mensagem é enviada em massa (ou automática); evita auto-replies e confirmações de leitura
+            "X-Auto-Response-Suppress": "All", // Solicita ao servidor do destinatário (como Outlook/Exchange) que **não envie nenhuma resposta automática**
         },
+
         subject: `Re: ${subject || `Novo contato de ${name}`}`,
         text: plainTextMessage,
         html: htmlMessage /*plainTextMessage.replace(/\n/g, "<br>")*/,
